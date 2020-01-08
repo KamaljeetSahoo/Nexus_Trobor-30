61b5cfacaef2ae2ba771614609bf438b59d2bb3c
KamaljeetSahoo <kamaljeetsahoo@gmail.com>
2020-01-08 17:23:40 +0530

    video

--- nexus.py
+++ nexus.py
@@ -18,8 +18,10 @@ while(True):
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    edged = cv2.Canny(gray, 30, 200)
-    contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    
+    #edged = cv2.Canny(gray, 30, 200)
+    contours, hierarchy = cv2.findContours(blurred, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     #cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
 
     cY_dict = {}
